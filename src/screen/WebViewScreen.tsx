11b5105502a5b58c8c7b7be70da7a3ac4d8a8dca
YounTaekSeon <youneciohc@gmail.com>
2025-03-10 15:54:03 +0900

    feat: 웹뷰에서 뒤로가기 시 버튼 alert 활성화

--- src/screen/WebViewScreen.tsx
+++ src/screen/WebViewScreen.tsx
@@ -1,8 +1,32 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Alert, BackHandler} from "react-native";
 import WebView from "react-native-webview";
-import { NativeHelper } from "../util/NativeHelper";
 
 const WebViewScreen = () => {
+    useEffect(() => {
+        const backAction = () => {
+            Alert.alert('주의', '정말 앱을 종료하시겠습니까?', [
+                {
+                    text: '아니요',
+                    onPress: () => null,
+                    style: 'cancel',
+                },
+                {
+                    text: '예', 
+                    onPress: () => BackHandler.exitApp()
+                },
+            ]);
+            return true;
+        };
+
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            backAction,
+        );
+
+        return () => backHandler.remove();
+    }, []);
+
     return (
         <WebView
         source={{uri: 'http://mtecsoft.co.kr:5800/pms/mobile/HpCheck.do?hpAuthNum=$'}}
