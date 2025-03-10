0675629ffd72af9ae8b118ac4e05ae42f46a1a03
YounTaekSeon <youneciohc@gmail.com>
2025-03-10 15:54:39 +0900

    feat: 뒤로가기 시 dialog 띄우는 기능 작성성

--- src/util/NativeHelper.ts
+++ src/util/NativeHelper.ts
@@ -1,35 +1,51 @@
-import { PermissionsAndroid, Platform } from "react-native";
+import { Alert, NativeModules, PermissionsAndroid, Platform } from "react-native";
 import DeviceInfo from "react-native-device-info";
 import SimCardsManager from "react-native-sim-cards-manager";
 import { KToast } from "../component/KToast";
+import { CommonDialog } from "../component/CommonDialog";
+import { DialogResult } from "../model/Types";
+import { sleep } from "./utils";
 
 export class NativeHelper {
-    static async getPhoneNumber(): Promise<string | null> {
-        if(Platform.OS !== 'android') {
-            return '';
-        } 
-        
-        try {
-            // 권한 요청
-            const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE);
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-              const simInfo = await SimCardsManager.getSimCards();
-              if (simInfo.length > 0 && simInfo[0].number) {
-                return simInfo[0].number;
-              }
-            }
-          } catch (error) {
-            KToast.showToast('휴대폰 번호 가져오기 실패');
-            return '';
-          }
 
+  async handleBackPress() {
+    const {InitiationModule} = NativeModules;
+
+    const confirmed = await CommonDialog.shotwAsync('종료', '앱을 종료 하시겠습니까?', true);
+    if(confirmed === DialogResult.Confirmed) {
+        await sleep(100);
+
+        InitiationModule.finishApp();
+    }
+  }
+
+  static async getPhoneNumber(): Promise<string | null> {
+      if(Platform.OS !== 'android') {
+          return '';
+      } 
+      
+      try {
+          // 권한 요청
+          const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE);
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            const simInfo = await SimCardsManager.getSimCards();
+            if (simInfo.length > 0 && simInfo[0].number) {
+              return simInfo[0].number;
+            }
+          }
+        } catch (error) {
+          KToast.showToast('휴대폰 번호 가져오기 실패');
           return '';
         }
 
-    static getDeviceModel(): any {
-        const modelNm = DeviceInfo.getModel;
-        const uID = DeviceInfo.getUniqueId;
+        return '';
+      }
 
-        return {modelNm: String, uID: String};
-    }
+  static getDeviceModel(): any {
+      const modelNm = DeviceInfo.getModel;
+      const uID = DeviceInfo.getUniqueId;
+
+      return {modelNm: String, uID: String};
+  }
 }
+
